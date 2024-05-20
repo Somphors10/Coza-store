e23522eb456cef33e02f1854df768f8ce291b2a8
Som phors <somphorstae9@gmail.com>
2024-05-20 12:39:42 +0700

    live

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../componets/Navbar'
+import AllProduct from '../componets/AllProduct'
+import Footer from '../componets/Footer'
+import OurBlog from '../componets/OurBlog'
+import CategoryProduct from '../componets/CategoryProduct'
+
+const Shop = () => {
+  return (
+    <div>
+      <Navbar/>
+      <AllProduct/>
+      <OurBlog/>
+      <CategoryProduct/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Shop
